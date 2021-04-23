9f99c0ea9407cd97864c3af7c267f0cdf0790b73
Arli <arli.machado@gmail.com>
2021-04-22 22:50:19 -0300

    Iniciando dashboard

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  Input as ChakraInput,
+  FormControl,
+  FormLabel,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps): JSX.Element {
+  return (
+    <FormControl>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <ChakraInput
+        name={name}
+        type="email"
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bgColor: 'gray.900' }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
