52d765204bd62dd4283a447148fd8e92ff110766
Arli <arli.machado@gmail.com>
2021-04-23 21:31:48 -0300

    Componentizando a aplicação

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile(): JSX.Element {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps): JSX.Element {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Arli Machado</Text>
-        <Text color="gray.300" fontSize="small">
-          arli.machado@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Arli Machado</Text>
+          <Text color="gray.300" fontSize="small">
+            arli.machado@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Arli Machado"
